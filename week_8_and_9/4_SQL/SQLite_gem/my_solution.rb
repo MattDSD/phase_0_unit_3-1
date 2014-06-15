bb464186dc7707e45e978caeabd8988b75776fcc
Puru Dahal <pdahal@me.com>
2014-06-15 01:41:12 -0500

    Solution :octocat: Reflection :thumbsup:

--- week_8_and_9/4_SQL/SQLite_gem/my_solution.rb
+++ week_8_and_9/4_SQL/SQLite_gem/my_solution.rb
@@ -1,6 +1,6 @@
 # U3.W7: BONUS Using the SQLite Gem
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge with: Matt Darin
 
 require 'sqlite3'
 
@@ -10,17 +10,25 @@ $db = SQLite3::Database.open "congress_poll_results.db"
 
 def print_arizona_reps
   puts "AZ REPRESENTATIVES"
-  az_reps = $db.execute("SELECT name FROM congress_members WHERE location = 'AZ'")
-  az_reps.each { |rep| puts rep }
+  puts $db.execute("SELECT name FROM congress_members WHERE location = 'AZ'")
+  #az_reps.each { |rep| puts rep }
 end
 
 def print_longest_serving_reps(minimum_years)  #sorry guys, oracle needs me, i didn't finish this!
   puts "LONGEST SERVING REPRESENTATIVES"
-  puts $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  reps = $db.execute("SELECT name,years_in_congress FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  reps.map{|name, years| (puts "#{name} - #{years}")}
+	#reps.map{|name, years| (puts "#{name}"+" - " + "#{years}}  
 end
 
-def print_lowest_grade_level_speakers
-  puts "LOWEST GRADE LEVEL SPEAKERS (less than < 8th grade)"
+def print_lowest_grade_level_speakers(grade_level)
+  puts "LOWEST GRADE LEVEL SPEAKERS (less than < #{grade_level})"
+  puts $db.execute("SELECT name FROM congress_members WHERE grade_current < #{grade_level}")
+end
+
+def print_reps_from_nj_ny_me_fl_ak
+	puts "LIST OF REPRESENTATIVES FROM New Jersey, New York, Maine, Florida, and Alaska"
+	puts $db.execute("SELECT name FROM congress_members WHERE location IN ('NJ', 'NY', 'ME', 'FL', 'AK')")
 end
 
 def print_separator
@@ -39,12 +47,13 @@ print_longest_serving_reps(35)
 # output should look like:  Rep. C. W. Bill Young - 41 years
 
 print_separator
-print_lowest_grade_level_speakers 
+print_lowest_grade_level_speakers(8)
 # TODO - Need to be able to pass the grade level as an argument, look in schema for "grade_current" column
 
 # TODO - Make a method to print the following states representatives as well:
 # (New Jersey, New York, Maine, Florida, and Alaska)
-
+print_separator
+print_reps_from_nj_ny_me_fl_ak
 
 ##### BONUS #######
 # TODO (bonus) - Stop SQL injection attacks!  Statmaster learned that interpolation of variables in SQL statements leaves some security vulnerabilities.  Use the google to figure out how to protect from this type of attack.
@@ -60,10 +69,13 @@ print_lowest_grade_level_speakers
 
 
 # REFLECTION- Include your reflection as a comment below.
-# How does the sqlite3 gem work?  What is the variable `$db` holding?  
+# How does the sqlite3 gem work?  What is the variable `$db` holding?
+=> $db has access to all the data on database.
 # Try to use your knowledge of ruby and OO to decipher this as well as h
 # ow the `#execute` method works.  Take a stab at explaining the line 
 # `$db.execute("SELECT name FROM congress_members WHERE years_in_congress 
 #   > #{minimum_years}")`.  Try to explain this as clearly as possible for 
+=> This SQL query selects all the names from tha table congress_members who have served more than minimum_years, minimum_years being 
+the number of years thats been passed in the method as an argument.
 # your fellow students.  
 # If you're having trouble, find someone to pair on this explanation with you.
